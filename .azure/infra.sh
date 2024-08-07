3111401f7fbc974d4386118e8b08be1495028bf0
Isaac Mendoza <imendozak24@gmail.com>
2024-08-07 01:42:15 -0400

    init 2

---
+++ .azure/infra.sh
@@ -0,0 +1,15 @@
+az login
+az ad app create --display-name mvpx-genai
+az ad sp create --id 4d430b10-268a-475f-9c69-05833ebee3e9
+az role assignment create --role contributor --subscription d0de32f6-0f62-4455-9d75-8a21aa89a1e2 --assignee-object-id  f80d93b2-8222-47d9-9d28-3daa86eb6574 --assignee-principal-type ServicePrincipal --scope /subscriptions/d0de32f6-0f62-4455-9d75-8a21aa89a1e2/resourceGroups/rg-mpvx-genai-backend
+az ad app federated-credential create --id 8a0df03c-7569-4eea-ba26-e9d20d430c25 --parameters credential.json
+("credential.json" contains the following content)
+{
+    "name": "oidc-github-geshuro-mvpx-genai",
+    "issuer": "https://token.actions.githubusercontent.com",
+    "subject": "repo:geshuro/template-genai-backend-python:ref:refs/heads/develop",
+    "description": "oidc-github-geshuro-mvpx-genai",
+    "audiences": [
+        "api://AzureADTokenExchange"
+    ]
+}
--- HttpTrigger1/__init__.py
+++ HttpTrigger1/__init__.py
@@ -12,7 +12,7 @@ app = FastAPI()
 
 @app.get('/')
 def read_root():
-    return {'Hello': 'World'}
+    return {'Hello': 'Pirula'}
 
 @app.get('/items/{item_id}')
 def read_item(item_id: int, q: str = None):
