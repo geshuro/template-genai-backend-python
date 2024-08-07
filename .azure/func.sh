c4fc708cc5e7de426baa1fea1ff740c58aae33e0
Isaac Mendoza <imendozak24@gmail.com>
2024-08-07 02:49:47 -0400

    init 5

---
+++ .azure/func.sh
@@ -0,0 +1,3 @@
+#https://learn.microsoft.com/en-us/azure/azure-functions/functions-run-local?tabs=macos%2Cisolated-process%2Cnode-v4%2Cpython-v2%2Chttp-trigger%2Ccontainer-apps&pivots=programming-language-python
+func init --worker-runtime python --model V2
+func new --template "Http Trigger" --name MyHttpTrigger
